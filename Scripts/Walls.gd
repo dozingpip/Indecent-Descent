a3a20d43cb8059d657feae28d6b7ddbe33926965
Aquosking <aquosking202@hotmail.com>
2019-08-12 21:45:01 -0400

    Make the wall lights higher and put in correct folder
    The wall lights weren't shining on the levels. And the light was way too
    bright. The light was also in the wrong folder. Donezo.

--- Scripts/Walls.gd
+++ Scripts/Walls.gd
@@ -1,21 +1,22 @@
 extends Spatial
 
 var light_dist_from_wall = 5
+var light_height = 0.8
 
 # make a box out of 4 cubes
 func makeWalls(wall_height, width, length):
 	$Wall0.set_translation(Vector3(-(width/2), 0, 0))
 	$Wall0.set_scale(Vector3(1, wall_height, length))
-	get_node("Wall0/Light").translate(Vector3(light_dist_from_wall, 0, 0))
+	get_node("Wall0/Light").translate(Vector3(light_dist_from_wall, light_height, 0))
 
 	$Wall1.set_translation(Vector3((width/2), 0, 0))
 	$Wall1.set_scale(Vector3(1, wall_height, length))
-	get_node("Wall1/Light").translate(Vector3(-light_dist_from_wall, 0, 0))
+	get_node("Wall1/Light").translate(Vector3(-light_dist_from_wall, light_height, 0))
 
 	$Wall2.set_translation(Vector3(0, 0, (length/2)))
 	$Wall2.set_scale(Vector3(width, wall_height, 1))
-	get_node("Wall2/Light").translate(Vector3(0, 0, -light_dist_from_wall))
+	get_node("Wall2/Light").translate(Vector3(0, light_height, -light_dist_from_wall))
 
 	$Wall3.set_translation(Vector3(0, 0,-(length/2)))
 	$Wall3.set_scale(Vector3(width, wall_height, 1))
-	get_node("Wall3/Light").translate(Vector3(0, 0, light_dist_from_wall))
+	get_node("Wall3/Light").translate(Vector3(0, light_height, light_dist_from_wall))
